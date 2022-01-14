38650a020641d202e823236ab448f00d2130260e
jsegu447 <jsegu010@ucr.edu>
2022-01-14 13:56:52 -0800

    Created repo

---
+++ grading/plane.cpp
@@ -0,0 +1,44 @@
+#include "plane.h"
+#include "ray.h"
+#include <cfloat>
+#include <limits>
+
+// Intersect with the half space defined by the plane.  The plane's normal
+// points outside.  If the ray starts on the "inside" side of the plane, be sure
+// to record a hit with t=0 as the first entry in hits.
+Hit Plane::Intersection(const Ray& ray, int part) const
+{
+    //TODO; IN PROGRESS
+    vec3 xe = ray.endpoint - this->x1;
+    double t;
+    Hit inter;
+    if(dot(this->normal,ray.direction) != 0){
+	t = dot(this->normal, xe)/ dot(this->normal,ray.direction);
+	
+	inter.object = this;
+	inter.dist = t;
+	inter.part = -1;
+	}   
+
+	else{
+		inter.object = nullptr;
+		inter.dist = -1;
+		inter.part = -1;
+	}
+    return inter;
+}
+
+vec3 Plane::Normal(const vec3& point, int part) const
+{
+    return normal;
+}
+
+// There is not a good answer for the bounding box of an infinite object.
+// The safe thing to do is to return a box that contains everything.
+Box Plane::Bounding_Box(int part) const
+{
+    Box b;
+    b.hi.fill(std::numeric_limits<double>::max());
+    b.lo=-b.hi;
+    return b;
+}
